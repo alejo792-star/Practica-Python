cb8f58ee096c6bec8c96832f94796795bfdb20d0
Alejandro Martinez <alejo792@icloud.com>
2026-02-05 07:09:23 -0800

    Prueba de desviacion estandar

--- Python Basico/5. Estructuras de datos/5.1. Listas/ejercicios calculos listas.py
+++ Python Basico/5. Estructuras de datos/5.1. Listas/ejercicios calculos listas.py
@@ -1,10 +1,23 @@
-edades = [23,43,45,213,34,66,77,88,345,567,345,67,234,67,78,978,4,534]
+import math
 
+edades = [23,43,45,213,34]
 
 sumador = 0
+promedio = 0
 
 for uwu in range( len(edades) ):
     sumador = sumador +   edades[uwu]
+promedio = sumador/len(edades)
+sumador =sumador
+
+almacenador =0
+for i in range(len(edades)):
+    res1= (edades[i] - promedio)**2
+    almacenador = almacenador + res1
+
+almacenador=(len(edades) - 1/4)* almacenador
+resutadof =math.sqrt(almacenador)
+
 print(sumador)
 
 
