ac59cacc589491dc56cb415a94be86c912a1f80d
Alejandro Martinez <alejandromartinez@Mac.lan>
2026-02-05 06:10:47 -0800

    Calculo de suma de la lista

--- Python Basico/5. Estructuras de datos/5.1. Listas/ejercicios calculos listas.py
+++ Python Basico/5. Estructuras de datos/5.1. Listas/ejercicios calculos listas.py
@@ -1,8 +1,8 @@
 edades = [23,43,45,213,34,66,77,88,345,567,345,67,234,67,78,978,4,534]
 
-awdaw
-delattrd
-af
-andsd
-assertda
-sd
+
+sumador = 0
+
+for uwu in range( len(edades) ):
+    sumador = sumador +   edades[uwu]
+print(sumador)
